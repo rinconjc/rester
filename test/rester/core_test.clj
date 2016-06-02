4fbeedd054748eb8b4968ba7ef6389e4c5fe4f47
Julio Rincon <jcrinconc@gmail.com>
2016-06-03 07:42:27 +1000

    diff tests

--- test/rester/core_test.clj
+++ test/rester/core_test.clj
@@ -1,5 +1,5 @@
 (ns rester.core-test
-  (:require [clojure.data.xml :refer [sexp-as-element]]
+  (:require [clojure.data.xml :refer [indent-str sexp-as-element]]
             [clojure.test :refer :all]
             [rester.core :refer :all]))
 
@@ -19,4 +19,6 @@
 
   (testing "diff xmls"
     (is (nil? (diff* (sexp-as-element [:a {:name "abc" :other "10"} [:b [:c]] [:d]])
-                     (sexp-as-element [:a {:name "abc" :other "10" :more "zzz"} [:x] [:b [:c]] [:d]]))))))
+                     (sexp-as-element [:a {:name "abc" :other "10" :more "zzz"} [:x] [:b [:c]] [:d]]))))
+    (is (= [(sexp-as-element [:b [:c]])] (diff* (sexp-as-element [:a {:name "abc" :other "10"} [:b [:c]] [:d]])
+                            (sexp-as-element [:a {:name "abc" :other "10" :more "zzz"} [:x] [:d]]))))))
