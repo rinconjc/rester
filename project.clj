a40aa77b6e4e0237dda4a7fbf0db8af17e6be617
Julio Rincon <jcrinconc@gmail.com>
2018-12-20 08:01:26 +1100

    fixes

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.csv "0.1.3"]
                  [clj-http "2.3.0"]
                  [org.clojure/tools.logging "0.3.1"]
