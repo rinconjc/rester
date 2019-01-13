bd85b19f7b5c7db99abee930488041336eccbefa
Julio Rincon <jcrinconc@gmail.com>
2019-01-14 09:42:19 +1100

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rester "0.1.0-SNAPSHOT"
+(defproject rester "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
