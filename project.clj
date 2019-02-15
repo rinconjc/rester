dd56f01ae2110ad00d5cbd87fb8a42895c1e7f59
jrincon2 <julio.rincon@macquarie.com>
2019-02-15 11:06:24 +1100

    Fix curl formatting

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rester "0.2.1-SNAPSHOT"
+(defproject rester "0.2.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
